8403a9ba8fb3b97e8febf041fb53b93ffcc4ea61
Cherhazad <cherhazad.boustani@outlook.fr>
2024-06-10 20:16:48 +0200

    TP6 Dates - Exercice 7

--- src/test/java/java17/ex07/DateAndTime_07_Test.java
+++ src/test/java/java17/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,11 @@ package java17.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.junit.Test;
@@ -18,19 +21,25 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+		@SuppressWarnings("deprecation")
+		Date date = new Date(117, 01, 12);
 
 		// TODO transformer la date en Instant
+		Instant dateInstant = date.toInstant();
 
 		// TODO transformer la date en LocalDate
 
-		LocalDate result = null;
+		ZoneId currentZone = ZoneId.systemDefault();
+		LocalDate result = LocalDate.ofInstant(dateInstant, currentZone);
 
+		System.out.println(result);
+		
 		// TODO valoriser les différentes variables afin de rendre le test passant
 
 		assertThat(result.getYear(), is(2017));
 		assertThat(result.getMonth(), is(Month.FEBRUARY));
 		assertThat(result.getDayOfMonth(), is(12));
+		
 
 	}
 }
