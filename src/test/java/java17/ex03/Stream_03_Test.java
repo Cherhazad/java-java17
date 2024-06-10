8b901f63a65be95e5b14e407dfbfe8542d95f58b
Cherhazad <cherhazad.boustani@outlook.fr>
2024-06-10 15:32:20 +0200

    TP5 - exercice 3 pas terminé

--- src/test/java/java17/ex03/Stream_03_Test.java
+++ src/test/java/java17/ex03/Stream_03_Test.java
@@ -30,7 +30,8 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Retrouver la commande avec le prix le plus élevé
-		Optional<Order> result = null;
+		Optional<Order> result = orders.stream().max(Comparator.comparing(o->o.getPrice()));
+		System.out.println(result);
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(2200.0));
@@ -42,7 +43,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Retrouver la commande avec le prix le moins élevé
-		Optional<Order> result = null;
+		Optional<Order> result = orders.stream().min(Comparator.comparing(o->o.getPrice()));
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.get().getPrice(), is(1000.0));
@@ -55,7 +56,8 @@ public class Stream_03_Test {
 
 		// TODO construire une chaîne contenant les prénoms des clients triés et séparés
 		// par le caractère "|"
-		String result = null;
+		String result = customers.stream().map(c->c.getFirstname()).sorted().collect(Collectors.joining("|"));
+		System.out.println(result);
 
 		assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
 	}
@@ -66,7 +68,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Extraire la liste des pizzas de toutes les commandes
-		List<Pizza> result = null;
+		List<Pizza> result = orders.stream().flatMap(o->o.getPizzas().stream()).toList();
 
 		assertThat(result.size(), is(9));
 	}
@@ -77,7 +79,7 @@ public class Stream_03_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Extraire la liste des différentes pizzas de toutes les commandes
-		List<Pizza> result = null;
+		List<Pizza> result = orders.stream().flatMap(o->o.getPizzas().stream()).distinct().toList();;
 
 		assertThat(result.size(), is(4));
 	}
